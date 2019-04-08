ab70bcfbf6bbb63fcb463a24d364c101283402c8
alexisspa9 <alexisspa9@gmail.com>
2019-04-08 12:30:04 +0300

    added pages tests and minitest reporters

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Twitter Micropost Clone"
+  end
+
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
