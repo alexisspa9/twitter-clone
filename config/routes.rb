ee6546754f86acec5e28c48692ba0aef40b36451
alexisspa9 <alexisspa9@gmail.com>
2019-04-08 13:50:29 +0300

    Configures User signup and set app production server

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 	get '/about', to: "pages#about"
 	get '/contact', to: "pages#contact"
 	get  '/signup',  to: 'users#new'
+    post '/signup',  to: 'users#create'
+	resources :users
 end
